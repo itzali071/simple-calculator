5d9df449eb7e75b8a831594f870b84c9a36276c5
Muhammad Ali Raza <maliraza70gb@gmail.com>
2024-04-26 22:03:51 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First number:", type: "number", name: "FirstNumber" },
+  { message: "Enter Second number:", type: "number", name: "SecondNumber" },
+  { 
+    message: "Select the operation:", 
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Subtraction","Multiplication","Division"],
+    },
+]);
+
+// conditional statement
+if (answer.operators === "Addition"){
+    console.log(answer.FirstNumber + answer.SecondNumber)
+    }
+else if(answer.operators === "Subtraction"){
+    console.log(answer.FirstNumber - answer.SecondNumber)
+    }
+else if(answer.operators === "Multiplication"){
+    console.log(answer.FirstNumber * answer.SecondNumber);
+    }
+else if(answer.operators === "Multiplication"){
+    console.log(answer.FirstNumber * answer.SecondNumber);
+    }
+else if(answer.operators === "Division"){
+    console.log(answer.FirstNumber / answer.SecondNumber);
+    }
+else{
+    console.log("Select a Valid Operaton");
+    }
